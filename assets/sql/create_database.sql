15a809c5af331b942cff5d2726d14062d5c59a96
marcoweb <marcocarvalho.web@gmail.com>
2021-11-12 11:34:15 +0000

    Criação do Banco de Dados

--- assets/sql/create_database.sql
+++ assets/sql/create_database.sql
@@ -1,2 +1,11 @@
 -- Código SQL para a criação da estrutura do banco de dados
 -- Lembre-se de alterar a configuração 'dbname' ('config/database.php') para refletir o nome do banco criado
+
+CREATE DATABASE exemplo;
+
+USE exemplo;
+
+CREATE TABLE filmes(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    titulo VARCHAR(255)
+);
--- assets/sql/init_data.sql
+++ assets/sql/init_data.sql
@@ -1,1 +1,7 @@
 -- Código SQL com a inclusão dos registros iniciais do banco de dados
+
+INSERT INTO filmes(titulo) VALUES
+    ('Entre Facas e Segredos'),
+    ('O Hobbit'),
+    ('Guia do Mochileiro das Galáxias'),
+    ('Monty Python: Em Busca do Cálice Sagrado');
